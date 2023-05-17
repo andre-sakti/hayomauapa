99eb3968ab4c21229209e741e86d9a4c8370bab0
andre-sakti <133019194+andre-sakti@users.noreply.github.com>
2023-05-17 20:10:55 +0700

    Delete setup.sh

--- setup.sh
+++
@@ -1,72 +0,0 @@
-֭xZ'ܺ~ǜȥz{al(	^rX+zڵw6{((^rH'P-r>xDND^jǑtĜ*mh,z_^jxjx.jD4gDL_,FPzID
-hߢ*'D@}7aCt}7f,}y-A4}7cD}ߙ`t}7bt}7cB}y7_u{]juwh+y}e{]ܛ~眆}Y}&&眆}Y}&e}Y}&!}m7Iyל}Y}&ZwJ֭'~jبDNENDЈ4SXE8Ў(FDA
-84#42
-,4cBHCC`B9d43BD@!QYD4/X
-fXM ё7CC4G,z,'Qk"ǫrz{\oڝƤ/zIzQk"ǫrz{\oڝƤ/hf'>zޯw^yԜ*my+ZnW^zxrW*'TDR uƚ+{H<],"wf=˫!lw芏.'ǕjXjا!wHb.'Ǌ{"'.'ǿ)(^rHejy"|ˉǧ{+azw>W'KǧО+u~/
-).'ǕjXj׊{"'.'ǿ)'N)NZkO+Zw?ȚjZ\Zj[bX(+fhjX(+fjG_v_v_vv76}5v!zr>.˭ߟ(Z۞r>˜q,jX(+fjGZ۞g.~Rq{(Z۞rr>xa+q,~e
-y^u:zޮ^zV֭y[Fzbjֿ:zޭ˫!lDK֭y߫zwbq{m.'ǿ)윺ߝb|g)6k'ˉǧ{+azyv*s.!z{\'Ǥ{'8.'ǕjXjםWW眆b.'Ǌ{#hVױ_$x= et͢'>^!DX<],6Czל^+uz[y"�ڵy?VX-jӉ֤rm?'QVXmq{ɬ$[kkwy?VX-jӉ֤ azwMŢuקbTj^zpeubW䤊k~'Xb+\~mIzQ)m.'ǿ)sHZ_ˊhy._izޯ/ږ'Z~"׫yߥǧ{+q
-ާǧ{'.~h)쵩eםyh94Z+n']zv(qxƱ.)쵩e{ly)"yz{)z֢ˉǧ7j)Sjx j_zfޖ&z{u^ߝu^&IzZȲ˞wqy˫!lDK֭y߫zwbq{mq{˭ߟvk&zG._izޯ/ږ'Z~"׫yߥǧ{+q
-ާǧ{'.~{kkƬkڒˉǧzZjjZ֭{fzIֲ>HZ_ˊhrm?'Qbu^wނ+-zXz{G`~z{r~wvӊ.'ǜ~mIzDjXjם^~'Ǿ+z.'ǿ)윺ߝ-k:n)"wfr>x`z{O*^Gb*'58{qxGb*'4/:iqx6)yhqx,mdDZȲ˞wqxОr>xa+ب+Zם:qCSjx j_zfޖ&z{4'8Jr*'w^tDA
-1q{^V4'8"{-jYZ*'.)dD'֥+U=#Byh'֥bu,jDA
-)S4/xƘyڵڞڞZֲ>LjƿKbڕInjzؤu^jK.'ǿjKIj/MI6jojۚ޶)j׾u6'jjȥڞ6؟6v'ڕInjzؤ1	-jV&
-bڞڞjV&
-b:M"|v_zx.jj1	-jj"x
-Z7OKazwb.'ǄƘyӧD@jƿ-.z{.&	bW眆8b.'Ǌ{("ޞjxڮ/jƿKbڟڮ/Ȟ/jV&
-bm^!1ڲ)=ຶ
-Ƭk$'
-6jojۚ޶)
-6ly!DA
-"ޞjxڮ/jƿKbڟڮ/Ȟ/jV&
-bЃk+y_V֭y+q,~eyh>&�4ІDA
-{^Ђ`MZ+J֫)"{-jYZ*'yh]؟޵񭬿v{azw%yr>xE8vxr7y^u!4S}.Eeiȧ'+@P
-Z:tOIh槺iߩ䮭嚞vzZq䜮*mcchxrW!4SxzZr)Iץ'+azw%yr>xE9+Zا"*moگbo?鞖'hmr>xE8-jw^+uzXx'yh
-*'	zW{ly!DXȞ"ūx+u~'%y߉!DA
-J֫)"{-jYZ*'4/F-*r+-ȞZT^zg:(Ʃ)쵩eɢϲjbZ[.vjbZ[(q~ɪm{-jYeɪm{-jYc)^j%yܕrW^j%yܕrWҵxz۩w4ݢwjx.j	![Byh'u')zx vjp虨>i؞r>xZs.:&j)ζ+r>xhs.:&j)Ϯ&v")DA
-Оr>y֧&)"x"gjFsf)DA
-Оr>y,4'iߩyhCHb
-(B`B9mЍ	hǿvUbub�.'?>jע}hǿv}K"�.'fҶ^笴6j˛ׯڝ&qay~s0
-~~V(I@� .nZlt! <"ק4f&_kiׇu5ㇽq[ۭ{,zߞbڊRrĢ}'~(&y!J]jا3R~P85˫\aiڦ'%_rrX'jg86Zrد{&j)Bua_
-z{~O*^jeƭ'."u9ܺ~8m?brZ.uZ(%?Ή޳N4Oݞy+vڙ0
-uaIP~)c¢{^ߓʗbqb㲉z˥HDCW^9ܺ~9$lhjoܖ':'z8?v{?(>~f! ?_.{ pק橦XjبuZ*^vy%3�\{^uJku޲mDD-7._-sNmj~Vy&bz{wgrݳDCuaIP~)c¢{^ߓʗbqb㲉֭jܩx	ڙ0
-rz{H>em)byږǞrRP86iX+kzorP87ݢf!P87گbo?鿊'~^zrzYh!}m7IzYh:&j)uםWwhq-&j)r>xa.:&j)ݢf^zB|Z+I^rبW!&j)yܕrW^j%yܕrW^j4ݢwjx.j	![Byh䢕"ا:&j)֊x='R{n&j|H:&j)x)hjr>xؚ6mdjpy+t7=Syh,Оr?v)b"znwHbwhhrW!DM޵-yױ_ȞtTثj剿r!&j)z?azݢfmyh hX+kzolryhv뢋vwhq-&j)ҹǜ8bwhWo[眆>Wࡢc'_jH''q^rWi\!DA
- N4$jb
-ޝzl!(Fxzwjا''q^Gb-&z\alZY޶Xɪi&z~rg!rw-&ziɞ뢋iɞrgﮦ
-ujh`^(!&z~ǭןjmƾǫz뢋iɞrg˞h-jwZGnz&ziɞ{-jY\z~u&j)ZzZ޵񭬿ƶr~Ga\kk)qkk(֥!砭 N4 y"%yuyh+y4S	')]j׬ժm{-jY\lƭ?)lj~+m `{h'5v{.)l^jǾɪm{-jYlƭr'm!&W綗Zﲞ]jשg+m(֥k|gjל[ym(֥^ezv!v̬霶Z޲֫k|+-zg-nW'̬霶Z޲֫k|gjל[b,jF!r.Ǭl!rrHڦا֥˫ zz)z{,wq\ƤxZ%uEثg?i\'>uZ>uZZ h|0Z0Z޵񭬜jj񭬭!Z q,-!Z h?ƶz6-!Z t(^rC슉z?ujz"{az{(\>^jǎHynbz?qϫzWaz{(\>^jǎIǧ'Jۭڦا֥젞`ƾqj׬^jǮnʸ˫mƊG|('x${()y梿셪G)|~j!쫊x 
-k{_u	w+Z{*?r{ʸ
-`i)?$jfX{{n^jǾr|z?jq%]x"bk{_=$j>)("x"+s➷yx"ϊ*+ܽO޵ڦޭz|Z۩u^&)쵩e)|fث޵^^|lzqi)}''L^q+Zۊ_޵^^|lzq'^rzX9'^n&jbZ_{gǫ'q^[yujb{*.Xm?x"+)Ƞ)젦u櫚{ʸ)܆+G)))}כ('yߛ!j쫊x,x"b{*)mƊyby[y')\m.Ǭ+-w|ei)|y|h	ϊzޕz|g>)>+rM?z?jzqg)jn֭{욦ا֥짂)Gbz?+-zg+-zo)ǝzq1yĭjOn)WMz?+-zg+-zo)ǝޮy'}^euzZvǫ'Ʒǧ)zv\x"׬nWןjmo޵)ȭzϚi^ןjm޵)x"(m?+az(ק&IݑO,rlf|\wثg?ip~;s|rw'~m nn'rwdkm.*m?lr&n)Wz"tޭ~.m+a(-/׭{-jY?)쵩ey֥j־*'ןmڟ�3tM,�3tM,,@43]4K(rK �3tM,
-q
->M4^@43]4Kz �3tM/,DC8ϵM4g
-q
->M4荩஗�3tM)$kjw~mz\Zn(J)z{,wڱת^qz?ƶ\j޵)'}񭬜w8[+-z4+-z4+-z~8%iڷhbקvz+ǫnYhqal_zb~'޵񭬿ƶrl_zb~'{/޵񭬿ƶ쬲_܊^1p
-@�!O@
- >�D1ioH-vA4۞ RKnzE >�D1 !1ipIpH-%2ҿ]S-+2ҿ]8[w袖*'s|rwkk'(w\x"ܢw'~ǝ(r\x"ܢw'~ǝ(rm۽4׽4\x"ܢw'~ǝ(rmޮȨ]z?(ƶr߱~*kzEzj-{v޵)'}񭬜wyߢ/zy^םZ+1w~j^ws|rwkk'(w袦1^i׫R+iס+i׿z?(ƶr߱~*kzw*'+i׿z?(ƶr߱~*kzmz?(ƶr߱~/޵)'}񭬜wyߢZjبz?z?(ƶr߱~/޵)'}񭬜wyߢ/yثyh}޵)'}񭬜wyߢ/j!]Mo,\x"ܢw'~ǝ(rmޮȨ]z?(ƶr߱~*kzEzj-{v޵)'}񭬜wyߢ/zy^םZ+1w~j^ws|rwkk'(w袦1^i׫R+iס+i׿z?(ƶr߱~*kzw*'+i׿z?(ƶr߱~*kzmz?(ƶr߱~/޵)'}񭬜wyߢZjب(rK>޵)'}񭬜wyߢs|rwkk'(w袦1v*rs|rwkk'(w袦1Zmn[(rK0s|rwkk'(w袦1mz"}u\x"ܢw'~ǝ(rǭu騵uz?(ƶr߱~*kzj^wh/u+yߢ޵)'}񭬜wyߢ/zy^J`^i`^\x"ܢw'~ǝ(rǭu¢yrب`^\x"ܢw'~ǝ(rǭuǢa-\x"ܢw'~ǝ(z?(ƶr߱~)hqb,\x"ܢw'~ǝ(z?(ƶr߱~*kb-z?(ƶr߱~*k,ivM5gs|rwkk'(w袦1mz"}u\x"ܢw'~ǝ(rǭu騵uz?(ƶr߱~*kzj^wh/u+yߢ޵)'}񭬜wyߢ/zy^J`^i`^\x"ܢw'~ǝ(rǭu¢yrب`^\x"ܢw'~ǝ(rǭuǢa-\x"ܢw'~ǝ(z?(ƶr߱~)hqbڟ,\x"ܢw'~ǝ(z?(ƶr߱~*kb-z?(ƶr߱~*k,ivM5#j|,\x"ܢw'~ǝ(rmޮȨ]z?(ƶr߱~*kzEzj-{v޵)'}񭬜wyߢ/zy^םZ+1w~j^ws|rwkk'(w袦1^i׫R+iס+i׿z?(ƶr߱~*kzw*'+i׿z?(ƶr߱~*kzmz?(ƶr߱~/޵)'}񭬜wyߢZjب,?z?(ƶr߱~/޵)'}񭬜wyߢ/yثyh}޵)'}񭬜wyߢ/j!]Ml?z?(ƶr߱~*ki*'_޵)'}񭬜wyߢ/zy^y~ ޚ^]s|rwkk'(w袦1^i׫_hu~鮌r]ߢם+\x"ܢw'~ǝ(rǭuԦ
-um
-u޵)'}񭬜wyߢ/zy^*'-
-u޵)'}񭬜wyߢ/zy^z,mh޵)'}񭬜wyߢs|rwkk'(w袖*'^Ϡ?z?(ƶr߱~/޵)'}񭬜wyߢ/yثyh}޵)'}񭬜wyߢ
-s^i׫_^j_>׿ik\x"ܢw'~ǝ(?zj^wh/u+yߢ޵)'}񭬜wyߢ
-s^i׫-i,s|rwkk'(w袂\
-s۽4^+޵)'}񭬜wyߢs|rwkk'(w袖*'Ϡ?z?(ƶr߱~/޵)'}񭬜wyߢ/yثyh}޵)'}񭬜wyߢ
-s^i׫_^j_>׿ik\x"ܢw'~ǝ(?zj^wh/u+yߢ޵)'}񭬜wyߢ
-s^i׫-i,s|rwkk'(w袂\
-s۽4+޵)'}񭬜wyߢs|rwkk'(w袖*'Ϡ?z?(ƶr߱~/޵)'}񭬜wyߢ/yثyh}޵)'}񭬜wyߢ
-s^i׫_^j_>׿ik\x"ܢw'~ǝ(?zj^wh/u+yߢ޵)'}񭬜wyߢ
-s^i׫-i,s|rwkk'(w袂\
-s۽4+޵)'}񭬜wyߢs|rwkk'(w袖*'荩\\x"ܢw'~ǝ(z?(ƶr߱~*kb-z?(ƶr߱~(+zy^y~ ޚ^]s|rwkk'(w袂\ǭuZ+y߅kiu~z?(ƶr߱~(+zy^z,mh޵)'}񭬜wyߢ
-sZ+n[k6\\x"ܢw'~ǝ(z?(ƶr߱~)hqb+޵)'}񭬜wyߢs|rwkk'(w袦1v*rs|rwkk'(w袂\ǭu騵uz?(ƶr߱~(+zy^םZ+1w~j^ws|rwkk'(w袂\ǭuǢa-\x"ܢw'~ǝ(?,\vM5+޵)'}񭬜wyߢs|rwkk'(vuƭs2wvƶrߊskk/ܢw;(CCqǬگhkk/qǬZ zگhkk/ޮ+%	^pjnvbקnZhtihr]Jzj>v+mx,ikz5۽4֠j^]Mio,-%Wǭ)ל*mw'ȝ蝾Wkyzb'z(L,ĞاZ,+-z}vM5g趇(ا%'n'Z׫!/,jdا޷
-+0')ಖz%^]Mimڟ)hrmڞǭ)ל*mw'Z(ۮk6˝zkyzb'z(L,ĞاZڟ,+-z}vM5$)hrl,mx,ja,i({lǫn(⥫,^,ޗKzkyzb'z(L,ĞاZ,%^]Mil)hrlhاɞMZi>o>ɩj0izWbz{l,n(K,޷
-+)l')w,Imx,a>bקnZhz )hro,mx,u+ʛby%'n'o,\jdا޷
-+
-r
-q')ǫ'5W+bקnZhz )hro,mx,rX"vumz+-Imx,p  zb,zqZz>\+-z}vM5#jx+k[k6mx,u+ʛby%
-+vv荩஗,')w䂺\\Imx,Ǎjgڟ+bקnZh(rK )hrl,mx,ja,i({lǫn(⥫,^+^tm笶')w䂺\\Imx,Ǎjg,
-rX۽4֚+ )hrlhاɞMZi>o>ɩj0izWbz{l,n(K,\p-rkyzb'z(H++Ğاx֦z>\[ݲ趇(y&mx,-%nV%zǭ)֠nZkb
--ʗ]4OM45NO^۞4z׽M?m}M4n5m6ݵ4^|O_|]tOm7]wv4{?J..ځhrGvwSjnu6jʗ]*^~'vnu6nZihr[hZƩǫ'+ZĞz֠jXWz[4֭K:0XzZ,z))䶻+-zk-j.))䶻֭ۢ')䶻֭nuJey-笵l:[')䶻\̬̬ƶǝqD8_޵^^kk+x7r*'^Iǃ˦z{Z*'inm&ez޷
-+֫gϥ.֫Rzq,z0Z+rv)IB�?
-4DC!Tfz{BjX�?
-4DC!T6^)^-+Zj_ۊ񭬫wskk/ܢw;(j)n^֫޽J֭ͷ.)8-tMh4H,MtM4Љ쵩eYyrmzZ\yh잖TERБzZ%ǫ'WW!	G0֭k0֭kh鹹bsm[%yuyh잖TERБzZ`|Wz?xw|kzZ%yu+-zg-֞hi%yuyh+yЄzƶ+-zg-nW^e-jqzf!y}&yh+yȞZR^-4'{}}^zpalܢ{^(v-ZʉZfޙ-!~7nlޙ-֥!gyhm7Ir>z
-zr'֥$d	!y}&Wg%yalܢ{^(v-ZʉZf,\j,~! zm?+az(ק&ݭjKbuڵj)^\jllǭn:0Zt6hМw9]!lܢ{^(v-ZʉZfw, zߪw,m?+az(ק&ݭjKbfj]0p~u߬,ik"ǫrz{\oڝƤ/k*&jku߬, zߪwz!lܢ{^(v-ZʉZfwz,9]lܢ{^(v-ZʉZfwk zߪ%ym?+az(ק&ݭjKbfj^jhp~z{Wm nn'rjwk{ؿj)^!m?+az(ק&ݭjKbfjgz,:g(rK!lܢ{^(v-ZʉZfﬡ, zߪ鞞rJalܢ{^(v-ZʉZfrJl:g,ik"ǫrz{\oڝƤ/k*&jkz{!荩lܢ{^(v-ZʉZfﭮڞp~z{alܢ{^(v-ZʉZfגּ!ǫ!lܢ{^(v-ZʉZfzp~z{lܢ{^(v-ZʉZf!Ƙm?+az(ק&ݭjKbfjl9$m nn'rjwk{ؿj)m)!~ŷ$f߱i~!~ŧ]0f߱i~$f߱i~Wf߱i~!~wrw\櫖jﯕ,rwfz{gf߱jﬡ,rwfz{#jw!~g,f߱$f߱rwq\j޵ܮپBO1ƥۊ};-n-j޵ܮپBO1ƥۊ}-+D47s+wr_o44n)Lƥhqn)+mkn)rWZ jgo4>4;y螷ׯ{mlzq+zޖׯ{m\q-~)^W.[.wrj؛h)e'!m!azjkqg{k%yz{47!z뢋W-Z )쵩emZ[qz?ir߶o޵}wߊ'sh+y^Z yh.�Prsh+y^Z ~(b+qٛXjo-n^1lz�1r>z
-zsyj+T֭x0M$x=$^ntܺ͠~m~*B=˫!l/b+m+z B<Ғ*m瞖Ǡ%wl"rjwk{ؿ)(b+ׯ{ƭ\zǧ!i<ƬkڒˉǧzZjjZhXq-X"'~(>	hmnޙjwkzzv	W(rߊZj[zڙv-"x+Zu"&+~h"ڝ~2f騵v)l"rjwk{ؿ)!m?iu<hOh^)H}P̵_&ܢoڝƤ/h+\uzwln)쵩eL7NOwι]��zN>~R5cx(Al+?t^y~ 
-5&jب'آâf&j)ȞZVjדjx j_zfޖ&z{.'ǄƓjK.'ǿjKIj"qy:$zr'.'ǿ)싉ǧȸz{.'ǔjf".yˉǧT=˫>\NmjzWۢ(Cݙ,juZpvסi~vױ�zY^r5hz-X._J'ܢwgz?(rr>4ϭ颺orr>rrrzq^{楢Z[q!ztmy>{-jYmלHun|EjZ )쵩e^r>I!Hnx{^{楢Z[q!䭺yޗ8jZ )쵩e^r>)mOu{^{楢Z[q!Zvg]4tמh觲֥myh"5jZ )쵩e^r>]Vg2y>{-jYmלD,6Lמh觲֥myht@aY^K7jZ )쵩e^r>]VW'y2K^{楢Z[q!6y>{-jYmלڝd{^{楢Z[q!v(dy>{-jYmל^{楢Z[q!'ȝ+bsyn-y>{-jYmלgw&%ֆ1?jZ )쵩e^r>8^{楢Z[q!ߕ^8^{楢Z[q!zÍjZ )쵩e^r>hN6מh觲֥myhEjZ )쵩e^r>hӧjoM c^{楢Z[q!.rK
-ZjZ )쵩e^r>h*)e2m u,z^{楢Z[q!.ޕ^b@q.^{楢Z[q!n\&j؜^{楢Z[q!OJǭ)מh觲֥myhfpמh觲֥myh(Zmy>{-jYmלzhڵ^{楢Z[q!nSujĞz^{楢Z[q!!!!!䜮*m
-AӋ!rr-y>{-jYm(l-7nl-!gv޵񭬿vxb+r>잖UD
-Ѓ*.n-2zi֧j{0z2{lz_
