cb26b0d376c52bfc746c2b19500af6ad56fbd311
andre-sakti <133019194+andre-sakti@users.noreply.github.com>
2023-05-25 12:51:08 +0700

    Update menu-socks.sh

--- menu-socks.sh
+++ menu-socks.sh
@@ -1,40 +1,63 @@
 #!/bin/bash
 
-clear
+BIBlack='\033[1;90m'      # Black
+BIRed='\033[1;91m'        # Red
+BIGreen='\033[1;92m'      # Green
+BIYellow='\033[1;93m'     # Yellow
+BIBlue='\033[1;94m'       # Blue
+BIPurple='\033[1;95m'     # Purple
+BICyan='\033[1;96m'       # Cyan
+BIWhite='\033[1;97m'      # White
+UWhite='\033[4;37m'       # White
+On_IPurple='\033[0;105m'  #
+On_IRed='\033[0;101m'
+IBlack='\033[0;90m'       # Black
+IRed='\033[0;91m'         # Red
+IGreen='\033[0;92m'       # Green
+IYellow='\033[0;93m'      # Yellow
+IBlue='\033[0;94m'        # Blue
+IPurple='\033[0;95m'      # Purple
+ICyan='\033[0;96m'        # Cyan
+IWhite='\033[0;97m'       # White
+NC='\e[0m'
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+# // Exporting Language to UTF-8
+
+export LANG='en_US.UTF-8'
+export LANGUAGE='en_US.UTF-8'
+
+
 # // Export Color & Information
-export RED='\033[0;31m';
-export GREEN='\033[0;32m';
-export YELLOW='\033[0;33m';
-export BLUE='\033[0;34m';
-export PURPLE='\033[0;35m';
-export CYAN='\033[0;36m';
-export LIGHT='\033[0;37m';
-export NC='\033[0m';
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[0;33m'
+export BLUE='\033[0;34m'
+export PURPLE='\033[0;35m'
+export CYAN='\033[0;36m'
+export LIGHT='\033[0;37m'
+export NC='\033[0m'
 
 # // Export Banner Status Information
-export ERROR="[${RED} ERROR ${NC}]";
-export INFO="[${YELLOW} INFO ${NC}]";
-export OKEY="[${GREEN} OKEY ${NC}]";
-export PENDING="[${YELLOW} PENDING ${NC}]";
-export SEND="[${YELLOW} SEND ${NC}]";
-export RECEIVE="[${YELLOW} RECEIVE ${NC}]";
-export RED_BG='\e[41m';
+export EROR="[${RED} EROR ${NC}]"
+export INFO="[${YELLOW} INFO ${NC}]"
+export OKEY="[${GREEN} OKEY ${NC}]"
+export PENDING="[${YELLOW} PENDING ${NC}]"
+export SEND="[${YELLOW} SEND ${NC}]"
+export RECEIVE="[${YELLOW} RECEIVE ${NC}]"
 
 # // Export Align
-export BOLD="\e[1m";
-export WARNING="${RED}\e[5m";
-export UNDERLINE="\e[4m";
+export BOLD="\e[1m"
+export WARNING="${RED}\e[5m"
+export UNDERLINE="\e[4m"
 
-# // ing URL Host
-export ROOT_DIRECTORY="/etc/bahenol";
-export CORE_DIRECTORY="/usr/local/bahenol";
-export SERVICE_DIRECTORY="/etc/systemd/system";
-export Server_URL="red-flat.my.id/vpn-script";
-export Server1_URL="red-flat.my.id/vpn-script";
-export Server_Port="443";
-export Server_IP="underfined";
-export Script_Mode="Stable";
-export Auther="bahenol";
+# // Exporting URL Host
+export Server_URL="raw.githubusercontent.com/andre-sakti/test/main"
+export Server1_URL="raw.githubusercontent.com/andre-sakti/limit/main"
+export Server_Port="443"
+export Server_IP="underfined"
+export Script_Mode="Stable"
+export Auther=".bijipeler"
 
 # // Root Checking
 if [ "${EUID}" -ne 0 ]; then
