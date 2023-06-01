b3a5702dcb4174bbc6ff4a78a6ff52e29fc5f13e
andre-sakti <133019194+andre-sakti@users.noreply.github.com>
2023-06-01 16:48:39 +0700

    Update menu-trojan.sh

--- menu-trojan.sh
+++ menu-trojan.sh
@@ -192,15 +192,6 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/config.json")
     if [ -z $user ]; then
     menu
     else
-    read -p "Expired (days): " masaaktif
-    exp=$(grep -wE "^### $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
-    now=$(date +%Y-%m-%d)
-    d1=$(date -d "$exp" +%s)
-    d2=$(date -d "$now" +%s)
-    exp2=$(( (d1 - d2) / 86400 ))
-    exp3=$(($exp2 + $masaaktif))
-    exp4=`date -d "$exp3 days" +"%Y-%m-%d"`
-    sed -i "/### $user/c\### $user $exp4" /etc/xray/config.json
     sed -i '/#trojanws$/a\### '"$user $exp"'\
     },{"password": "'""$pwd""'","email": "'""$user""'"' /etc/xray/config.json
     sed -i '/#trojangrpc$/a\### '"$user $exp"'\
