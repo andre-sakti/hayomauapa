d82b1a15a24fee3a6f271defed7717ab1fe2b341
andre-sakti <133019194+andre-sakti@users.noreply.github.com>
2023-06-04 01:20:19 +0700

    Update add-tr.sh

--- add-tr.sh
+++ add-tr.sh
@@ -152,11 +152,11 @@ read -rp "Password: " -e pwd
 read -p "Expired (days): " masaaktif
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
 sed -i '/#trojanws$/a\### '"$user $exp"'\
-},{"password": "'""$pwd""'" >>> "email": "'""$user""'"' /etc/xray/config.json
+},{"password": "'""$pwd""'","email": "'""$user""'"' /etc/xray/config.json
 sed -i '/#trojangrpc$/a\### '"$user $exp"'\
-},{"password": "'""$pwd""'" >>> "email": "'""$user""'"' /etc/xray/config.json
+},{"password": "'""$pwd""'","email": "'""$user""'"' /etc/xray/config.json
 sed -i '/#trojantcp$/a\### '"$user $exp"'\
-},{"password": "'""$pwd""'" >>> "email": "'""$user""'"' /etc/xray/config.json
+},{"password": "'""$pwd""'","email": "'""$user""'"' /etc/xray/config.json
 sed -i '/#trojanxtls$/a\#&# '"$user $exp"'\
 },{"password": "'""$pwd""'","flow": "'""xtls-rprx-direct""'","email": "'""$user""'"' /etc/xray/config.json
 
