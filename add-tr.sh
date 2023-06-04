6ba5ab6b0d285ceba493d9ff066402ffac480d70
andre-sakti <133019194+andre-sakti@users.noreply.github.com>
2023-06-04 16:16:55 +0700

    Update add-tr.sh

--- add-tr.sh
+++ add-tr.sh
@@ -152,13 +152,13 @@ read -rp "Password: " -e pwd
 read -p "Expired (days): " masaaktif
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
 sed -i '/#trojanws$/a\### '"$user $exp"'\
-},{"password": "'""$pwd""'","email": "'""$user""'"' /etc/xray/config.json
+},{"password": "'""$pwd""'" , "email": "'""$user""'"' /etc/xray/config.json
 sed -i '/#trojangrpc$/a\### '"$user $exp"'\
-},{"password": "'""$pwd""'","email": "'""$user""'"' /etc/xray/config.json
+},{"password": "'""$pwd""'" , "email": "'""$user""'"' /etc/xray/config.json
 sed -i '/#trojantcp$/a\### '"$user $exp"'\
-},{"password": "'""$pwd""'","email": "'""$user""'"' /etc/xray/config.json
+},{"password": "'""$pwd""'" , "email": "'""$user""'"' /etc/xray/config.json
 sed -i '/#trojanxtls$/a\#&# '"$user $exp"'\
-},{"password": "'""$pwd""'","flow": "'""xtls-rprx-direct""'","email": "'""$user""'"' /etc/xray/config.json
+},{"password": "'""$pwd""'" , "flow": "'""xtls-rprx-direct""'","email": "'""$user""'"' /etc/xray/config.json
 
 systemctl restart xray
 trojanlink3="trojan://${pwd}@${domain}:${tr}?mode=gun&security=tls&type=grpc&serviceName=trojan-grpc&sni=bug.com#${user}"
