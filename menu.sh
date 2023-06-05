81db6c5366d878d15d961be0c55dcfdf0cd1b33f
andre-sakti <133019194+andre-sakti@users.noreply.github.com>
2023-06-05 15:42:18 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -180,6 +180,62 @@ read -n 1 -s -r -p "Press any key to back on menu"
 menu
 fi
 }
+function x-bw(){
+clear
+_APISERVER=127.0.0.1:10000
+_XRAY=/usr/local/bin/xray
+
+apidata () {
+    local ARGS=
+    if [[ $1 == "reset" ]]; then
+      ARGS="-reset=true"
+    fi
+    $_XRAY api statsquery --server=$_APISERVER "${ARGS}" \
+    | awk '{
+        if (match($1, /"name":/)) {
+            f=1; gsub(/^"|link"|,$/, "", $2);
+            split($2, p,  ">>>");
+            printf "%s:%s->%s\t", p[1],p[2],p[4];
+        }
+        else if (match($1, /"value":/) && f){
+          f = 0;
+          gsub(/"/, "", $2);
+          printf "%.0f\n", $2;
+        }
+        else if (match($0, /}/) && f) { f = 0; print 0; }
+    }'
+}
+
+print_sum() {
+    local DATA="$1"
+    local PREFIX="$2"
+    local SORTED=$(echo "$DATA" | grep "^${PREFIX}" | sort -r)
+    local SUM=$(echo "$SORTED" | awk '
+        /->up/{us+=$2}
+        /->down/{ds+=$2}
+        END{
+            printf "SUM->up:\t%.0f\nSUM->down:\t%.0f\nSUM->TOTAL:\t%.0f\n", us, ds, us+ds;
+        }')
+    echo -e "${SORTED}\n${SUM}" \
+    | numfmt --field=2 --suffix=B --to=iec \
+    | column -t
+}
+
+DATA=$(apidata $1)
+echo "------------Inbound----------"
+print_sum "$DATA" "inbound"
+echo "-----------------------------"
+echo "------------Outbound----------"
+print_sum "$DATA" "outbound"
+echo "-----------------------------"
+echo
+echo "-------------User------------"
+print_sum "$DATA" "user"
+echo "-----------------------------"
+read -n 1 -s -r -p "Press any key to back on menu"
+menu
+fi
+}
 function cek-bandwidth(){
 clear
 echo -e ""
@@ -523,6 +579,7 @@ echo -e "${yl}│  $yy 3$wh. VLESS $wh     $yy[${wh}Menu$yy]         9$wh.  ADD-
 echo -e "${yl}│  $yy 4$wh. TROJAN $wh    $yy[${wh}Menu$yy]         10$wh. BACKUP$wh           $yy[${wh}Menu$yy]   ${yl}│${yl}" 
 echo -e "${yl}│  $yy 5$wh. SS WS $wh     $yy[${wh}Menu$yy]         11$wh. GEN SSL$wh          $yy[${wh}Menu$yy]   ${yl}│${yl}" 
 echo -e "${yl}│  $yy 6$wh. SOCK WS $wh   $yy[${wh}Menu$yy]         12$wh. RESTART SERVICE$wh  $yy[${wh}Menu$yy]   ${yl}│${yl}" 
+echo -e "${yl}│  $yy 13$wh. Bandwidth User$wh $yy[${wh}Menu$yy]"
 echo -e "${yl}│                                                              ${yl}│${yl}"
 echo -e "${yl}└──────────────────────────────────────────────────────────────┘${yl}"
 echo -e "${yl}┌──────────────────────────────────────────────────────────────┐${NC}"
@@ -549,7 +606,7 @@ case $opt in
 11) clear ; genssl ;;
 12) clear ; systemctl restart xray; systemctl restart ws-stunnel; systemctl restart nginx; systemctl restart fail2ban; systemctl restart dropbear; systemctl restart ssh; systemctl restart stunnel4;
     clear; echo -e "${OKEY} Successfull Restarted All Service";
-    ;;
+13) clear ; x-bw ;;
 #069) clear ; wget https://raw.githubusercontent.com/andre-sakti/ranjau-darate/main/update.sh && chmod +x update.sh && ./update.sh && rm -f /root/update.sh ;;
 0) clear ; menu ;;
 #x) exit ;;
