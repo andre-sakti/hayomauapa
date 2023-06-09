6a0fb1158f30254b2a1d45e5c6a4886b1bfa863c
andre-sakti <133019194+andre-sakti@users.noreply.github.com>
2023-06-10 00:28:06 +0700

    Update menu-vless.sh

--- menu-vless.sh
+++ menu-vless.sh
@@ -282,7 +282,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^###vls " "/etc/xray/config.json")
 	echo "Select the existing client you want to show the config"
 	echo " Press CTRL+C to return"
 	echo -e "==============================="
-	grep -E "^###vls " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | nl -s ') '
+	grep -E "^###vls " "/etc/xray/config.json" | cut -d ' ' -f 2-4 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
 		if [[ ${CLIENT_NUMBER} == '1' ]]; then
 			read -rp "Select one client [1]: " CLIENT_NUMBER
